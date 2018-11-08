8c3ba2a3bf5f7e2c6f58e34ace12eea0021eabf7
Luciano Júnior <luciannojunior@gmail.com>
2018-11-08 16:12:15 -0300

    Spinner

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import { Landing, Login, AppShell } from './pages'
 import PrivateRoute from './components/PrivateRoute'
 import firebase from 'firebase/app'
@@ -7,20 +8,26 @@ import 'firebase/auth'
 
 class AppRouter extends Component {
   state = {
-    user: null
+    user: null,
+    loading: true
   }
-  
+
   componentDidMount = () => {
     firebase.auth().onAuthStateChanged(user => {
-      this.setState({ user })
+      this.setState({ user, loading: false })
     })
   }
 
   render = () => {
-    const { user } = this.state
+    const { user, loading } = this.state
     return (
       <BrowserRouter>
         <div>
+          {loading && (
+            <div className="fixed z-9999 top-0 left-0 bottom-0 right-0 bg-black-20 justify-center ">
+              <CircularProgress style={{top: '50%', left: '50%', position: 'absolute'}} />
+            </div>
+          )}
           <Route path="/" exact component={Login} />
           <Route path="/login/" component={Login} />
           <PrivateRoute user={user} path="/app/" component={AppShell} />
