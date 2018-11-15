db4daf2bd364af37687f4caf58a158676c0e2835
Luciano Júnior <luciannojunior@gmail.com>
2018-11-15 11:38:13 -0300

    Scaffholding DevelopedBy

---
+++ src/components/DevelopedBy.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({});
+
+const DevelopedBy = ({}) => {
+    return (<div></div>)
+}
+
+export default withStyles(styles)(DevelopedBy)
